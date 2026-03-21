0d3434aaaba4e51d32efaa11b380f7a15dcd3b8f
an9383 <an9383@gmail.com>
2026-03-21 11:11:42 +0900

    all

--- src/modules/chat-archive/dto/lower-case.transformer.ts
+++
@@ -1,7 +0,0 @@
-//import { TransformFnParams } from 'class-transformer/types/interfaces';
-import { TransformFnParams } from 'class-transformer';
-import { MaybeType } from './maybe.type';
-
-export const lowerCaseTransformer = (
-  params: TransformFnParams,
-): MaybeType<string> => params.value?.toLowerCase().trim();
